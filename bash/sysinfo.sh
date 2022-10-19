221fff8500a128f00175cd6c7a8bad1b7eb76fe2
MeetPatel908 <meetp.9085790@gmail.com>
2022-10-18 20:45:55 -0400

    your commit comment

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+HNAME=$(hostname --fqdn) #variable Hname is assigned command to print hostname 
+OsName=$(hostnamectl |grep "Operating System") #varibale OsName is assigned command for showing Operating system name
+OsVersion=$(uname -r) #Variable Osversion is assigned command for showing version of system
+IpAdd=$(hostname -I) #veriable IpAdd is assigned command for showing Ip address 
+FreeSpace=$(df -h |awk 'NR==3{print $4}') #varible FreeSpace is assigned command for showing free space
+
 cat <<EOF
-Report for my $(hostname) 
+Report for my $HNAME 
 ========================
-FQDN : $(hostname -A)           
-version and $(hostnamectl |grep "Operating System") $(uname -r)
-IP Address : $(hostname -I)
-Root File system free space :
-$(df -h)
+FQDN: $HNAME          
+version and $OsName $OsVersion
+IP Address : $IpAdd
+Root File system free space :$FreeSpace
+
 ========================
 EOF
